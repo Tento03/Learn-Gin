4a6631da10da947fe9651727593cae0ca010e1ef
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-12-23 15:32:55 +0700

    update routes

--- auth-gorm/routes/authRoutes.go
+++ auth-gorm/routes/authRoutes.go
@@ -10,10 +10,10 @@ func UserRoutes(r *gin.Engine) {
 	r.POST("/login", controllers.Login)
 	r.POST("/register", controllers.Register)
 	r.POST("/refresh", controllers.RefreshToken)
-	r.GET("/me", controllers.RequireAuth, func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{"message": "login sukses"})
-	})
-	r.POST("/logout", controllers.RequireAuth, controllers.Logout, func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{"msg": "logout sukses"})
-	})
+
+	auth := r.Group("/")
+	auth.Use(controllers.RequireAuth)
+
+	auth.GET("/me", controllers.RequireAuth)
+	auth.GET("/logout", controllers.Logout)
 }
